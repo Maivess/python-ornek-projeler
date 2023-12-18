7de196cc4bfe59a4143e09b41c97019858b2d50c
Emre <133017995+Maivess@users.noreply.github.com>
2023-12-18 21:35:10 +0300

    Update hesap-makinesi.py

--- hesap-makinesi.py
+++ hesap-makinesi.py
@@ -14,16 +14,16 @@ class HesapMakinesi:
         if y != 0:
             return x / y
         else:
-            return "Bolme islemi sifira bolme hatasi"
+            return "Bölme işlemi sifira bolme hatasi"
 
     def faktoriyel(self, x):
-        max_limit = 170 
+        max_limit = 170  # math.factorial() sınırı
         if x == 0 or x == 1:
             return 1
         elif x < 0 or not isinstance(x, int):
-            return "Gecersiz giris. Pozitif bir tam sayi gerekir."
+            return "Geçersiz giriş. Pozitif bir tam sayı gerekir."
         elif x > max_limit:
-            return "Siniri astiniz. Daha kucuk bir sayi deneyin."
+            return "Sınırı aştınız. Daha küçük bir sayı deneyin."
         else:
             return math.factorial(x)
 
@@ -37,7 +37,7 @@ class HesapMakinesi:
                 raise OverflowError("Result too large")
         except OverflowError as e:
             if "Result too large" in str(e):
-                return "Siniri astiniz. Daha kucuk bir sayi deneyin."
+                return "Sınırı aştınız. Daha küçük bir sayı deneyin."
             else:
                 raise e
             
@@ -63,46 +63,48 @@ while True:
     print("\n1. Toplama")
     print("2. Çikarma")
     print("3. Çarpma")
-    print("4. Bolme")
-    print("5. Faktoriyel")
-    print("6. Us Alma")
-    print("7. Karekok")
+    print("4. Bölme")
+    print("5. Faktöriyel")
+    print("6. Üs Alma")
+    print("7. Karekök")
     print("8. Mod Alma")
-    print("9. Çikis")
+    print("9. Çıkış")
 
     secim = input("Yapmak istediğiniz işlemi seçin (1-9): ")
 
-    if not secim:
-        print("Geçersiz giriş. Lütfen bir seçenek girin.")
+    if not secim.isdigit() or not (1 <= int(secim) <= 9):
+        print("Geçersiz giriş. Lütfen 1 ile 9 arasında bir sayı girin.")
         continue
 
-    if secim == "9":
-        print("Çikis yapiliyor...")
+    secim = int(secim)
+
+    if secim == 9:
+        print("Çıkış yapılıyor...")
         break
 
     try:
-        sayi1 = float(input("Birinci sayiyi giriniz: "))
-        if secim != "5" and secim != "7":
-            sayi2 = float(input("İkinci sayiyi giriniz: "))
+        sayi1 = float(input("Birinci sayıyı giriniz: "))
+        if secim != 5 and secim != 7:
+            sayi2 = float(input("İkinci sayıyı giriniz: "))
     except ValueError:
-        print("Geçersiz giriş. Lütfen sayi giriniz.")
+        print("Geçersiz giriş. Lütfen sayı giriniz.")
         continue
 
-    if secim == "1":
+    if secim == 1:
         print("Toplama: ", hesap_makinesi.topla(sayi1, sayi2))
-    elif secim == "2":
-        print("Çikarma: ", hesap_makinesi.cikar(sayi1, sayi2))
-    elif secim == "3":
-        print("Carpma: ", hesap_makinesi.carp(sayi1, sayi2))
-    elif secim == "4":
-        print("Bolme: ", hesap_makinesi.bol(sayi1, sayi2))
-    elif secim == "5":
-        print("Faktoriyel: ", hesap_makinesi.faktoriyel(int(sayi1)))
-    elif secim == "6":
-        print("Us Alma: ", hesap_makinesi.us_alma(sayi1, sayi2))
-    elif secim == "7":
-        print("Karekok: ", hesap_makinesi.karekok(sayi1))
-    elif secim == "8":
+    elif secim == 2:
+        print("Çıkarma: ", hesap_makinesi.cikar(sayi1, sayi2))
+    elif secim == 3:
+        print("Çarpma: ", hesap_makinesi.carp(sayi1, sayi2))
+    elif secim == 4:
+        print("Bölme: ", hesap_makinesi.bol(sayi1, sayi2))
+    elif secim == 5:
+        print("Faktöriyel: ", hesap_makinesi.faktoriyel(int(sayi1)))
+    elif secim == 6:
+        print("Üs Alma: ", hesap_makinesi.us_alma(sayi1, sayi2))
+    elif secim == 7:
+        print("Karekök: ", hesap_makinesi.karekok(sayi1))
+    elif secim == 8:
         print("Mod Alma: ", hesap_makinesi.mod_alma(sayi1, sayi2))
     else:
-        print("Gecersiz seçim. Lutfen 1-9 arasinda bir secim yapin.")
+        print("Geçersiz seçim. Lütfen 1-9 arasında bir seçim yapın.")
